68ce74e5e0402897d31a4988bcc9ee6c6d0d808a
João Victor <jovictor.miguel@gmail.com>
2021-03-04 13:32:41 -0300

    test: ensure User model are valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validate user' do
+    it 'should be valid' do
+      user = create(:user)
+
+      expect(user).to be_valid
+    end
+  end
+
+  context 'Validate user fields' do
+    it 'name should not be valid' do
+      user = build(:user, :invalid_name)
+
+      expect(user).to_not be_valid
+    end
+    it 'email should not be valid' do
+      user = build(:user, :invalid_email)
+
+      expect(user).to_not be_valid
+    end
+    it 'password should not be valid' do
+      user = build(:user, :invalid_password)
+
+      expect(user).to_not be_valid
+    end
+    it 'admin should not be valid' do
+      user = build(:user, :invalid_admin)
+
+      expect(user).to_not be_valid
+    end
+  end
 end
